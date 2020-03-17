0e153598509e5e46e683418b6a3e32af993693c0
Marcos Olavo Silva Buganeme <marcos.after@gmail.com>
2020-03-16 23:17:54 -0300

    componente de mídia social (links de redes sociais)

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/marcosbuganeme'
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/marcosbuganeme'
+  },
+  {
+    label: 'Youtube',
+    url: 'https://youtube.com/marcosbuganeme'
+  },
+  {
+    label: 'Instagram',
+    url: 'https://instagram.com/marcosbuga'
+  },
+  {
+    label: 'Unsplash',
+    url: 'https://unsplash.com/@marcosbuganeme'
+  }
+]
+
+export default links
