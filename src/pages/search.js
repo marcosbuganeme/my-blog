ebd2fb1dff46e6f5c9adfab5a052d9eb1b367067
Marcos Olavo Silva Buganeme <marcos.after@gmail.com>
2020-03-19 00:03:08 -0300

    pagina de pesquisa

---
+++ src/pages/search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import SEO from '../components/seo'
+import Layout from '../components/Layout'
+import Search from '../components/Search'
+
+const SearchPage = () => (
+  <Layout>
+    <SEO title='Search' />
+    <Search />
+  </Layout>
+)
+
+export default SearchPage
