e00f88b03fcbc35770a148857c2032a4ba057c2d
Marcos Olavo Silva Buganeme <marcos.after@gmail.com>
2020-03-19 00:02:14 -0300

    configurando algolia para busca de posts

---
+++ src/utils/algolia.queries.js
@@ -0,0 +1,45 @@
+const PostsQuery = `{
+  posts: allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+    edges {
+      node {
+        objectID: id
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          category
+          background
+          description
+          date_timestamp: date
+          date(locale: "pt-br", formatString: "DD [de] MMM [de] YYYY")
+        }
+        excerpt(pruneLength: 5000)
+      }
+    }
+  }
+}`
+
+const flatten = array =>
+  array.map(({ node: { frontmatter, ...rest } }) => ({
+    ...frontmatter,
+    date_timestamp: parseInt(
+      (new Date(frontmatter.date_timestamp).getTime() / 1000).toFixed(0)
+    ),
+    ...rest
+  }))
+
+const settings = {
+  attributesToSnippet: [`excerpt:20`]
+}
+
+const queries = [
+  {
+    query: PostsQuery,
+    transformer: ({ data }) => flatten(data.posts.edges),
+    indexName: 'Posts',
+    settings
+  }
+]
+
+module.exports = queries
