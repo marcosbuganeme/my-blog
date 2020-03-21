dcf15c00c4062f179e2f0e93355923fbd31f2de4
Marcos Olavo Silva Buganeme <marcos.after@gmail.com>
2020-03-21 19:22:59 -0300

    adicionando shotname para integração do componente do disqs

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -11,7 +11,7 @@ const Comments = ({ url, title }) => {
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="marcosbuganeme"
+        shortname="marcosbuganeme-com-br"
         identifier={completeURL}
         title={title}
         url={completeURL}
